08e4e7a77407084c710593bccaf769074ca96fd4
holepratiksha29 <holepratiksha29@gmail.com>
2026-03-20 22:54:57 +0530

    Refactor PDF to image conversion using PyMuPDF

--- backend/app/ocr/pdf_to_image.py
+++ backend/app/ocr/pdf_to_image.py
@@ -1,20 +1,15 @@
-from pdf2image import convert_from_path
+import fitz  # PyMuPDF
 import os
 
 def pdf_to_images(pdf_path):
-    
-    poppler_path = os.path.join(os.getcwd(), "app", "tools", "poppler", "bin")
 
-    images = convert_from_path(
-        pdf_path,
-        poppler_path=poppler_path
-    )
-
+    doc = fitz.open(pdf_path)
     image_paths = []
 
-    for i, img in enumerate(images):
+    for i, page in enumerate(doc):
+        pix = page.get_pixmap()
         path = f"uploads/page_{i}.png"
-        img.save(path, "PNG")
+        pix.save(path)
         image_paths.append(path)
 
     return image_paths
