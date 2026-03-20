8178a5349ab6fd9d1b5205e94b6401ff99375853
holepratiksha29 <holepratiksha29@gmail.com>
2026-03-20 21:55:37 +0530

    Update pdf_to_image.py

--- backend/app/ocr/pdf_to_image.py
+++ backend/app/ocr/pdf_to_image.py
@@ -1,17 +1,15 @@
 import fitz  # PyMuPDF
 import os
 
-def pdf_to_images(pdf_path, output_folder="temp_images"):
+def pdf_to_images(pdf_path):
+    images = []
 
-    os.makedirs(output_folder, exist_ok=True)
+    pdf = fitz.open(pdf_path)
 
-    doc = fitz.open(pdf_path)
-    image_paths = []
-
-    for i, page in enumerate(doc):
+    for i, page in enumerate(pdf):
         pix = page.get_pixmap()
-        img_path = f"{output_folder}/page_{i}.png"
+        img_path = f"uploads/page_{i}.png"
         pix.save(img_path)
-        image_paths.append(img_path)
+        images.append(img_path)
 
-    return image_paths
+    return images
