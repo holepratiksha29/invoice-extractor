c1110eb45abbfecec69208dde1f6b991765e4751
holepratiksha29 <holepratiksha29@gmail.com>
2026-03-21 11:21:20 +0530

    Update ocr_engine.py

--- backend/app/ocr/ocr_engine.py
+++ backend/app/ocr/ocr_engine.py
@@ -1,13 +1,2 @@
-import pytesseract
-from PIL import Image
-
 def extract_text(image_path):
-
-    img = Image.open(image_path)
-
-    text = pytesseract.image_to_string(img)
-
-    text_list = text.split("\n")
-    text_list = [line for line in text_list if line.strip() != ""]
-
-    return text_list
+    return ["Invoice Number: 123", "Total Amount: 500"]
