58a2d0d49da8e2ff90c96e0ed61884fa4b2c7d20
holepratiksha29 <holepratiksha29@gmail.com>
2026-03-21 11:14:25 +0530

    Update ocr_engine.py

--- backend/app/ocr/ocr_engine.py
+++ backend/app/ocr/ocr_engine.py
@@ -2,7 +2,9 @@ import pytesseract
 from PIL import Image
 
 def extract_text(image_path):
+
     img = Image.open(image_path)
+
     text = pytesseract.image_to_string(img)
 
     text_list = text.split("\n")
